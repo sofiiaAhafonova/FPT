1cd89e61620a00ec6642188eee83d60dfcf65ca7
Your Name <you@example.com>
2018-04-23 00:09:41 +0300

    regex for brackets

--- lab3/src/main/java/com/labs/lab3/App.java
+++ lab3/src/main/java/com/labs/lab3/App.java
@@ -1,5 +1,5 @@
 package com.labs.lab3;
-
+import java.util.regex.*;
 /**
  * Hello world!
  *
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String myString = "(2*((3-2)+(5-3)))+(5*(3-7))";
+        String ar[] = myString.split("[\\(||\\)]");
+        int i = -1;
+        while (++i < ar.length)
+            System.out.println(ar[i]);
     }
 }
